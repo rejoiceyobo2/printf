da4557a8415779042af78bbf3165106e3a88b294
arohunmolase <68884461+arohunmolase@users.noreply.github.com>
2023-11-11 11:01:55 -0800

    Update print_functions1.c

--- print_functions1.c
+++ print_functions1.c
@@ -3,14 +3,15 @@
 /************************* PRINT UNSIGNED NUMBER *************************/
 /**
  * print_unsigned - Prints an unsigned number
- * by rejoice_yobo and Arohunmolase Daniel
- * @types: List a of arguments
- * @buffer: Buffer array to handle print
- * @flags:  Calculates active flags
- * @width: get width
- * @precision: Precision specification
- * @size: Size specifier
+ * by rejoice_yobo and Arohunmolase Daniel.
+ * @types: List a of arguments.
+ * @buffer: Buffer array to handle print.
+ * @flags:  Calculates active flags.
+ * @width: get width.
+ * @precision: Precision specification.
+ * @size: Size specifier.
  * Return: Number of chars printed.
+ * integers and statements.
  */
 int print_unsigned(va_list types, char buffer[],
 	int flags, int width, int precision, int size)
