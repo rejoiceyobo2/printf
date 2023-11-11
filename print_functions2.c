9046f73a52911b4a45a0f70faaeeb576e581ec30
arohunmolase <68884461+arohunmolase@users.noreply.github.com>
2023-11-11 11:03:39 -0800

    Update print_functions2.c

--- print_functions2.c
+++ print_functions2.c
@@ -3,14 +3,15 @@
 /****************** PRINT POINTER ******************/
 /**
  * print_pointer - Prints the value of a pointer variable
- * by rejoice_yobo and Arohunmolase Daniel
- * @types: List a of arguments
- * @buffer: Buffer array to handle print
- * @flags:  Calculates active flags
- * @width: get width
- * @precision: Precision specification
- * @size: Size specifier
+ * by rejoice_yobo and Arohunmolase Daniel.
+ * @types: List a of arguments.
+ * @buffer: Buffer array to handle print.
+ * @flags:  Calculates active flags.
+ * @width: get width.
+ * @precision: Precision specification.
+ * @size: Size specifier.
  * Return: Number of chars printed.
+ * integers and values.
  */
 int print_pointer(va_list types, char buffer[],
 	int flags, int width, int precision, int size)
