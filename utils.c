fd653574c65fd016d546d85d9deec787cc03968e
arohunmolase <68884461+arohunmolase@users.noreply.github.com>
2023-11-13 14:53:44 -0800

    Update utils.c

--- utils.c
+++ utils.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * is_printable - Evaluates if a char is printable
- * by rejoice_yobo and Arohunmolase Daniel
+ * is_printable - Evaluates if a char is printable.
+ * by rejoice_yobo and Arohunmolase Daniel.
  * @c: Char to be evaluated.
  *
- * Return: 1 if c is printable, 0 otherwise
+ * Return: 1 if c is printable, return 0 otherwise.
  */
 int is_printable(char c)
 {
@@ -13,11 +13,11 @@ int is_printable(char c)
 }
 
 /**
- * append_hexa_code - Append ASCII in hexadecimal code to buffer
+ * append_hexa_code - Append ASCII in hexadecimal code to buffer.
  * @buffer: Array of chars.
  * @i: Index at which to start appending.
  * @ascii_code: ASCII CODE.
- * Return: Always 3
+ * Return: Always 3.
  */
 int append_hexa_code(char ascii_code, char buffer[], int i)
 {
