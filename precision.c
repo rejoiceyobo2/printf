045c1899384a2dc7e99d2e20bfe34b363a0b8360
arohunmolase <gbengaarohunmolase@gmail.com>
2023-11-14 12:02:22 -0800

    updates

--- precision.c
+++ precision.c
@@ -2,51 +2,40 @@
 
 /**
  * get_precision - Calculates the precision for printing
- * @format: Formatted string containing conversion specifiers
- * @i: Pointer to the current position in the format string
- * @list: Variable argument list
+ * @format: Formatted string in which to print the arguments
+ * @i: List of arguments to be printed.
+ * @list: list of arguments.
  *
- * Return: Precision value:
- *   -1: No precision specified
- *    n: Precision value where n is a positive integer
+ * Return: Precision.
  */
 int get_precision(const char *format, int *i, va_list list)
 {
-	// Move to the character after '%'
 	int curr_i = *i + 1;
 	int precision = -1;
 
-	// Check if the precision specifier is present (indicated by the '.' character)
 	if (format[curr_i] != '.')
 		return (precision);
 
-	// Initialize precision to 0
 	precision = 0;
 
-	// Iterate through characters in the format specifier after the '.'
 	for (curr_i += 1; format[curr_i] != '\0'; curr_i++)
 	{
-		// If the current character is a digit, update the precision value
 		if (is_digit(format[curr_i]))
 		{
 			precision *= 10;
 			precision += format[curr_i] - '0';
 		}
-		// If the current character is '*', get precision from variable argument list
 		else if (format[curr_i] == '*')
 		{
 			curr_i++;
 			precision = va_arg(list, int);
 			break;
 		}
-		// Stop if a non-digit character is encountered
 		else
 			break;
 	}
 
-	// Update the position pointer to the last processed character
 	*i = curr_i - 1;
 
-	// Return the determined precision value
 	return (precision);
 }
--- utils.c
+++ utils.c
@@ -1,14 +1,13 @@
 #include "main.h"
 
 /**
- * is_printable - Checks if a character is printable
- * @c: Character to be evaluated.
+ * is_printable - Evaluates if a char is printable
+ * @c: Char to be evaluated.
  *
- * Return: 1 if the character is printable, 0 otherwise
+ * Return: 1 if c is printable, 0 otherwise
  */
 int is_printable(char c)
 {
-	// Check if the character falls within the printable ASCII range
 	if (c >= 32 && c < 127)
 		return (1);
 
@@ -16,25 +15,23 @@ int is_printable(char c)
 }
 
 /**
- * append_hexa_code - Appends ASCII in hexadecimal code to a buffer
- * @buffer: Array of characters.
+ * append_hexa_code - Append ascci in hexadecimal code to buffer
+ * @buffer: Array of chars.
  * @i: Index at which to start appending.
- * @ascii_code: ASCII code to be converted to hexadecimal.
+ * @ascii_code: ASSCI CODE.
  *
- * Return: Always 3 (length of the hexadecimal code)
+ * Return: Always 3
  */
 int append_hexa_code(char ascii_code, char buffer[], int i)
 {
 	char map_to[] = "0123456789ABCDEF";
-	/* The hexadecimal format code is always 2 digits long */
-
-	// Ensure non-negative representation
+	/* The hexa format code is always 2 digits long */
 	if (ascii_code < 0)
 		ascii_code *= -1;
 
-	// Append the escape sequence '\x' and the hexadecimal representation to the buffer
 	buffer[i++] = '\\';
 	buffer[i++] = 'x';
+
 	buffer[i++] = map_to[ascii_code / 16];
 	buffer[i] = map_to[ascii_code % 16];
 
@@ -42,14 +39,13 @@ int append_hexa_code(char ascii_code, char buffer[], int i)
 }
 
 /**
- * is_digit - Checks if a character is a digit
- * @c: Character to be evaluated
+ * is_digit - Verifies if a char is a digit
+ * @c: Char to be evaluated
  *
- * Return: 1 if the character is a digit, 0 otherwise
+ * Return: 1 if c is a digit, 0 otherwise
  */
 int is_digit(char c)
 {
-	// Check if the character is within the numeric ASCII range
 	if (c >= '0' && c <= '9')
 		return (1);
 
@@ -59,13 +55,12 @@ int is_digit(char c)
 /**
  * convert_size_number - Casts a number to the specified size
  * @num: Number to be casted.
- * @size: Size indicating the type to be casted.
+ * @size: Number indicating the type to be casted.
  *
  * Return: Casted value of num
  */
 long int convert_size_number(long int num, int size)
 {
-	// Cast the number based on the specified size
 	if (size == S_LONG)
 		return (num);
 	else if (size == S_SHORT)
@@ -75,15 +70,14 @@ long int convert_size_number(long int num, int size)
 }
 
 /**
- * convert_size_unsgnd - Casts an unsigned number to the specified size
+ * convert_size_unsgnd - Casts a number to the specified size
  * @num: Number to be casted
- * @size: Size indicating the type to be casted
+ * @size: Number indicating the type to be casted
  *
  * Return: Casted value of num
  */
 long int convert_size_unsgnd(unsigned long int num, int size)
 {
-	// Cast the unsigned number based on the specified size
 	if (size == S_LONG)
 		return (num);
 	else if (size == S_SHORT)
