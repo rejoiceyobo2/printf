92d873c48febe284ee5a3734c9f81fea751bc684
arohunmolase <68884461+arohunmolase@users.noreply.github.com>
2023-11-14 10:43:40 -0800

    Update flags.c

--- flags.c
+++ flags.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * get_flags - Calculates active flags
- * by rejoice_yobo and Arohunmolase Daniel
- * @format: Formatted string in which to print the arguments
+ * get_flags - Calculates active flags.
+ * by rejoice_yobo and Arohunmolase Daniel...
+ * @format: Formatted string in which to print the arguments.
  * @i: take a parameter.
  * Return: Flags:
  */
